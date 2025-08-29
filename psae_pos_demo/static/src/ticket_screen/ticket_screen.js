7bdaca1d91e53d1d1a3c9110e66ba9db6e5820c0
abwa <abwa@odoo.com>
2025-08-29 15:18:23 +0400

    [IMP] psae_pos_demo: 3. Display Order Type on TicketScreen

---
+++ psae_pos_demo/static/src/ticket_screen/ticket_screen.js
@@ -0,0 +1,10 @@
+/** @odoo-module */
+
+import { TicketScreen } from "@point_of_sale/app/screens/ticket_screen/ticket_screen";
+import { patch } from "@web/core/utils/patch";
+
+patch(TicketScreen.prototype, {
+    getOrderType(order) {
+        return order.orderTypeId;
+    }
+});
