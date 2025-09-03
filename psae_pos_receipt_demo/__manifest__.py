6f8dc0e2df144555debb35f0d85a3fd7b89ddcb1
abwa <abwa@odoo.com>
2025-09-03 11:56:28 +0400

    [ADD] psae_pos_receipt_demo: 1. Add Arabic Name to Contacts

---
+++ psae_pos_receipt_demo/__manifest__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Utilico - PoS Receipt Demo",
+    'summary': "Custom module for PoS Receipt Customization.",
+    'version': '17.0.0.1.0',
+    'depends': ['point_of_sale'],
+    'license': "OEEL-1",
+    'data': [
+        'views/res_partner_views.xml'
+    ],
+}
