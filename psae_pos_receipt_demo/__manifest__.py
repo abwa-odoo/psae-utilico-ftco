8015eb3b4d918c08a27dea5386b728995d9bbdd7
abwa <abwa@odoo.com>
2025-09-03 12:21:17 +0400

    [IMP] psae_pos_receipt_demo: 3. Display Arabic Name on Partner Screen

--- psae_pos_receipt_demo/__manifest__.py
+++ psae_pos_receipt_demo/__manifest__.py
@@ -5,6 +5,11 @@
     'version': '17.0.0.1.0',
     'depends': ['point_of_sale'],
     'license': "OEEL-1",
+    'assets': {
+        'point_of_sale._assets_pos': [
+            'psae_pos_receipt_demo/static/src/**/*',
+        ]
+    },
     'data': [
         'views/res_partner_views.xml'
     ],
