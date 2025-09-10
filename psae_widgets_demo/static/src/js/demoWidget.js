f6336bf5f205372129bed7d4917d4b4b6e0a659c
abwa <abwa@odoo.com>
2025-09-10 13:52:44 +0400

    [IMP] psae_widgets_demo: Improve Styling

--- psae_widgets_demo/static/src/js/demoWidget.js
+++ psae_widgets_demo/static/src/js/demoWidget.js
@@ -2,73 +2,131 @@
 
 import publicWidget from "@web/legacy/js/public/public_widget";
 
-
 publicWidget.registry.demoWidget = publicWidget.Widget.extend({
     selector: '#demo_widget',
     events: {
-        'click .remove-btn': '_onRemovePartner',
+        'click .o_partner_actions .btn-danger': '_onRemovePartner',
         'click .btn-add-partner': '_onAddPartner',
-        'click .btn-remove-result-msg': '_onRemoveResultMessage'
+        'click .btn-remove-result-msg': '_onRemoveResultMessage',
+        'submit #partner-form': '_onAddPartner'
     },
     init: function () {
         this._super.apply(this, arguments)
         this.orm = this.bindService('orm')
-        this.partner_list = $('.partner_list')
-        this.resultMsgSpan = $('#result-message')
-        this.crossIcon = $('.btn-remove-result-msg')
+        this.partner_list = $('.o_partner_list')
+        this.resultMessage = $('#result-message')
+        this.partnerInput = $('#partner_id')
+        this.partners = []
     },
     willStart: async function () {
         this._super.apply(this, arguments)
-        this.partners = await this.orm.searchRead("res.partner", [], ['name'], {limit: 5})
+        // Load initial partners (optional - you can remove this if you want to start with empty list)
+        try {
+            this.partners = await this.orm.searchRead("res.partner", [], ['name'], {limit: 5})
+        } catch (error) {
+            console.warn('Could not load initial partners:', error)
+            this.partners = []
+        }
     },
     start: function () {
-        this.partners.forEach(partner => {
-            let partnerItem = $(`
-                <div id="partner-${partner.id}" class="partner-item">
-                    ${partner.name} 
-                    <button class="btn btn-danger btn-sm remove-btn" data-id="${partner.id}">x</button>
+        this._super.apply(this, arguments)
+        this._renderPartnerList()
+    },
+    _renderPartnerList: function () {
+        this.partner_list.empty()
+        
+        if (this.partners.length === 0) {
+            this.partner_list.append(`
+                <div class="o_empty_state">
+                    <i class="fa fa-users"></i>
+                    <p>No partners added yet. Add a partner ID above to get started.</p>
                 </div>
             `)
-            this.partner_list.append(partnerItem)
-        })
+        } else {
+            this.partners.forEach(partner => {
+                this._addPartnerToDOM(partner)
+            })
+        }
+    },
+    _addPartnerToDOM: function (partner) {
+        // Remove empty state if it exists
+        this.partner_list.find('.o_empty_state').remove()
+        
+        let partnerItem = $(`
+            <div id="partner-${partner.id}" class="o_partner_item">
+                <div class="o_partner_info">
+                    <h5 class="o_partner_name">${partner.name}</h5>
+                    <p class="o_partner_id">ID: ${partner.id}</p>
+                </div>
+                <div class="o_partner_actions">
+                    <button class="btn btn-danger" data-id="${partner.id}" title="Remove partner">
+                        <i class="fa fa-trash"></i>
+                    </button>
+                </div>
+            </div>
+        `)
+        this.partner_list.append(partnerItem)
     },
     _onRemovePartner: function (e) {
-        let partnerId = Number(e.target.getAttribute('data-id'))
-        $(`#partner-${partnerId}`).remove()
-        this.partners = this.partners.filter(partner => partner.id !== partnerId)
+        e.preventDefault()
+        let partnerId = Number($(e.currentTarget).data('id'))
+        $(`#partner-${partnerId}`).fadeOut(300, () => {
+            $(`#partner-${partnerId}`).remove()
+            this.partners = this.partners.filter(partner => partner.id !== partnerId)
+            this._renderPartnerList()
+        })
     },
     _onAddPartner: async function (e) {
         e.preventDefault()
-        this.resultMsgSpan.addClass('d-none')
-        this.crossIcon.addClass('d-none')
-        let partnerId = Number($('.partner-input').val())
-        let isFound = this.partners.find(partner => partner.id === partnerId)
-        if (isFound) {
-            this.showResultMsg(`The partner is already on the list, ${isFound.name}`)
+        this._hideResultMessage()
+        
+        let partnerId = Number(this.partnerInput.val())
+        
+        // Validation
+        if (!partnerId || partnerId <= 0) {
+            this._showResultMessage('Please enter a valid partner ID', 'error')
             return
         }
-        let partner = await this.orm.searchRead("res.partner", [['id', '=', partnerId]], ['name'])
-        if (!partner.length) {
-            this.showResultMsg('The partner does not exist')
+        
+        // Check if partner already exists
+        let existingPartner = this.partners.find(partner => partner.id === partnerId)
+        if (existingPartner) {
+            this._showResultMessage(`Partner "${existingPartner.name}" is already in the list`, 'error')
             return
         }
-        debugger;
-        partner = partner[0]
-        this.partner_list.append($(`
-            <div id="partner-${partner.id}" class="partner-item">
-                ${partner.name} 
-                <button class="btn btn-danger btn-sm remove-btn" data-id="${partner.id}">x</button>
-            </div>
-        `))
-        this.partners.push(partner)
+        
+        // Show loading state
+        this._showResultMessage('Loading partner...', 'info')
+        
+        try {
+            let partners = await this.orm.searchRead("res.partner", [['id', '=', partnerId]], ['name'])
+            
+            if (!partners.length) {
+                this._showResultMessage(`Partner with ID ${partnerId} does not exist`, 'error')
+                return
+            }
+            
+            let partner = partners[0]
+            this.partners.push(partner)
+            this._addPartnerToDOM(partner)
+            this._showResultMessage(`Partner "${partner.name}" added successfully!`, 'success')
+            this.partnerInput.val('')
+            
+        } catch (error) {
+            console.error('Error loading partner:', error)
+            this._showResultMessage('Error loading partner. Please try again.', 'error')
+        }
     },
-    showResultMsg(msg) {
-        this.resultMsgSpan.text(msg)
-        this.resultMsgSpan.removeClass('d-none')
-        this.crossIcon.removeClass('d-none')
+    _showResultMessage: function (message, type = 'info') {
+        this.resultMessage.find('.o_message_text').text(message)
+        this.resultMessage.removeClass('o_success o_error').addClass(type === 'error' ? 'o_error' : 'o_success')
+        this.resultMessage.show()
     },
-    _onRemoveResultMessage: function () {
-        this.resultMsgSpan.addClass('d-none')
-        this.crossIcon.addClass('d-none')
+    _hideResultMessage: function () {
+        this.resultMessage.hide()
+    },
+    _onRemoveResultMessage: function (e) {
+        e.preventDefault()
+        this._hideResultMessage()
     }
 });
