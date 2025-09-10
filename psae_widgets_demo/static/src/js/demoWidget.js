98c134abfd56da6892b1db505b8fb3324d615dc6
abwa <abwa@odoo.com>
2025-09-10 13:21:43 +0400

    [IMP] psae_widgets_demo: Fetch + Display Partners

--- psae_widgets_demo/static/src/js/demoWidget.js
+++ psae_widgets_demo/static/src/js/demoWidget.js
@@ -3,13 +3,25 @@
 import publicWidget from "@web/legacy/js/public/public_widget";
 
 
-const demoWidget = publicWidget.Widget.extend({
+publicWidget.registry.demoWidget = publicWidget.Widget.extend({
     selector: '#demo_widget',
     init: function () {
         this._super.apply(this, arguments)
-        console.log("Demo Widget Loaded");
-    }
+        this.orm = this.bindService('orm')
+        this.partner_list = $('.partner_list')
+    },
+    willStart: async function () {
+        this._super.apply(this, arguments)
+        this.partners = await this.orm.searchRead("res.partner", [], ['name'], {limit: 5})
+    },
+    start: function () {
+        this.partners.forEach(partner => {
+            let partnerItem = $(`
+                <div id="partner-${partner.id}" class="partner-item">
+                    ${partner.name} 
+                </div>
+            `)
+            this.partner_list.append(partnerItem)
+        })
+    },
 });
-
-publicWidget.registry.demoWidget = demoWidget;
-// publicWidget.registry.demoWidget = publicWidget.Widget.extend({});
